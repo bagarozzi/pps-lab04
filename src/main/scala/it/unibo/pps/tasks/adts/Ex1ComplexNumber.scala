008c890ddef90ccff57ee333b10ee58b68ff9cb5
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2026-03-26 10:39:50 +0100

    Implemented exercise 1

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -20,11 +20,16 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    private case class ComplexNumber(re: Double, im: Double)
+    opaque type Complex = ComplexNumber
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.re
+      def im(): Double = complex.im
+      def sum(other: Complex): Complex = BasicComplexADT.complex(complex.re + other.re, complex.im + other.im)
+      def subtract(other: Complex): Complex = BasicComplexADT.complex(complex.re - other.re, complex.im - other.im)
+      def asString(): String = complex match
+        case ComplexNumber(re, 0.0) => re.toString
+        case ComplexNumber(0.0, im) => im.toString + "i"
+        case ComplexNumber(re, im) if im < 0 => re.toString + " - " + im.abs.toString + "i"
+        case _ => re.toString + " + " + im.toString + "i"
